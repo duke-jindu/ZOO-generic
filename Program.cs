98d4819ba648060135b10e2b27bf8908ee77d441
duke-jindu <dukechukwujindu71@gmail.com>
2023-04-20 16:41:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace AnimalSounds
+{
+    class Animal_Sound
+    {
+        public void  animalSound<T> (T Sound) 
+        {
+            Console.WriteLine(Sound);
+        }
+
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Animal_Sound Animal = new Animal_Sound();
+            Animal.animalSound("DOGS"); 
+            Animal.animalSound("Dogs bark"); 
+            Animal.animalSound(" "); 
+            Animal.animalSound("COWS"); 
+            Animal.animalSound("Cows Moo!"); 
+            Animal.animalSound(" "); 
+            Animal.animalSound("ELEPHANT"); 
+            Animal.animalSound("Elephants Trumpet"); 
+            Animal.animalSound(" "); 
+            Animal.animalSound("MONKEY"); 
+            Animal.animalSound("Monkeys laught?"); 
+            Animal.animalSound(" "); 
+            Animal.animalSound("HORSE"); 
+            Animal.animalSound("Horses neigh"); 
+            Animal.animalSound(" "); 
+            Animal.animalSound("LION"); 
+            Animal.animalSound("Lions Roar"); 
+            Animal.animalSound(" "); 
+            
+        }
+    }
+}
